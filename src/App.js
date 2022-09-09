fc220886ec8d213ba0f2cb20853d4e91faa12e46
alien <kabdualieva2002@gmail.com>
2022-09-09 16:47:51 +0600

    Make footer responsive

--- src/App.js
+++ src/App.js
@@ -198,21 +198,23 @@ function App() {
             <div className="rightCont">
               <ul className="lists">
                 <li><a href="#landings">Landings</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#product">Products</a></li>
-                <li><a href="#services">Services</a></li>
+                
+                <li><a href="#"  className='inside' >Home</a ></li>
+                <li><a href="#product" className='inside'>Products</a></li>
+                <li><a href="#services" className='inside'>Services</a></li>
+              
               </ul>
               <ul className="lists2">
                 <li><a href="#company">Company</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#help">Carees</a></li>
-                <li><a href="#services">Services</a></li>
+                <li><a href="#" className='inside'>Home</a></li>
+                <li><a href="#help" className='inside'>Carees</a></li>
+                <li><a href="#services" className='inside'>Services</a></li>
               </ul>
               <ul className="lists3">
                 <li><a href="#resourses">Resources</a></li>
-                <li><a href="#blog">Blog</a></li>
-                <li><a href="#product">Products</a></li>
-                <li><a href="#services">Services</a></li>
+                <li><a href="#blog" className='inside'>Blog</a></li>
+                <li><a href="#product" className='inside'>Products</a></li>
+                <li><a href="#services" className='inside'>Services</a></li>
               </ul>
             </div>
           </div>
