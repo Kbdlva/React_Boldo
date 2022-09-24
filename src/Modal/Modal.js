f115aa53ca36d1c2eed898345fd32134ef2ab263
Zhanbolat Bekmaganbetov <180107180@stu.sdu.edu.kz>
2022-09-24 15:40:52 +0600

    test

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,24 +1,55 @@
-import React from "react";
-import "./Modal.css"
+import React, { useState } from "react";
+import "./Modal.css";
 
-const Modal = ({active, setActive}) => {
-    return (
-        <div className={active? "popup active" : "popup"} onClick={() =>setActive(false)}>
-        <div className='popup_body' >
-          <div className={active? "popup_content active" : "popup_content"} onClick={e =>e.stopPropagation()}>
-            <div className='popoup_title'>
-                <h1>Login</h1>
-            </div>
-            <div className='popup_fields'>
-                <button className="btn btn-dark" >Username</button>
-                <button className="btn btn-dark">Password</button>
-                <button className="btn btn-basic">Login</button>
-             
-            </div>
+const Modal = ({ active, setActive }) => {
+  const [token, setToken] = useState();
+  const [user, setUser] = useState(null);
+  async function login() {
+    await fetch("/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ username: "admin", password: "admin" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setToken(data.token);
+      });
+    await fetch("/user/1", {
+      headers: {
+        "Content-Type": "text/plain",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {setUser(data)});
+  }
+
+  return (
+    <div
+      className={active ? "popup active" : "popup"}
+      onClick={() => setActive(false)}
+    >
+      <div className="popup_body">
+        <div
+          className={active ? "popup_content active" : "popup_content"}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="popoup_title">
+            <h1>Login</h1>
+          </div>
+          <div className="popup_fields">
+            <button className="btn btn-dark">Username</button>
+            <button className="btn btn-dark">Password</button>
+            <button className="btn btn-basic" onClick={() => login()}>
+              Login
+            </button>
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Modal;
